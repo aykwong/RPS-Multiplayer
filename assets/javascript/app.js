4662acf6c7ab9fd354ae237307128dee36cc364f
Alpha <aykwong93@gmail.com>
2018-07-01 11:12:03 -0400

    test

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBDPXt5k5IWvtxMkTJOw4lz5eDOJZYrP20",
+  authDomain: "rps-multiplayer-94c87.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-94c87.firebaseio.com",
+  projectId: "rps-multiplayer-94c87",
+  storageBucket: "",
+  messagingSenderId: "152671759232"
+};
+firebase.initializeApp(config);
+
